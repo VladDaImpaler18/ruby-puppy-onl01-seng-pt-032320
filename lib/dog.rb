88124a53e1128d1762f216caf9c0c0fa7905376f
Vladimir J <vladdaimpaler@protonmail.com>
2020-04-15 21:33:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,28 @@
 class Dog
   
+  attr_reader :name
+  
   @@all=[]
   
   def initialize(dogName)
     @name = dogName
+    save
+  end
+  
+  def self all
+    @@all
+  end
+  
+  def self clear_all
+    @@all = []
+  end
+  
+  def self print_all
+    @@all
+  end
+  
+  def save
+    @@all << self
   end
   
   
